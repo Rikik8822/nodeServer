d5e14db050d56e88f102c36b0ff6d04ad27db2db
Riki <rikik8822@gmail.com>
2025-01-27 19:15:31 +0200

    forth commit

--- config/db.js
+++ config/db.js
@@ -1,14 +1,20 @@
 import { connect } from "mongoose";
+mongoose.set('useFindAndModify', false);  // מניעת שימוש ב-findAndModify
+
 export const connectToDb = async (req, res) => {
     try {
-        let con = await connect(process.env.DB_URL,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 30000})
-        console.log("mongo db connected")
+        let con = await connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, // זמן הקצוב לחיבור
+            connectTimeoutMS: 30000, // זמן לחיבור עצמו
+            socketTimeoutMS: 30000 // זמן לשאילתות
+        });
+
+        console.log("MongoDB connected");
     }
     catch (err) {
-        console.log("cannot connect mongoDB ", err)
-        process.exit(1)
+        console.log("Cannot connect to MongoDB: ", err);
+        process.exit(1); // אם החיבור נכשל, סיים את הפעולה
     }
 }
